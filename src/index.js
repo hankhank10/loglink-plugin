d74fea3fff764146d5bbe29138348229ecfbc1d6
hankhank10 <m.a.hankinson@gmail.com>
2023-01-02 07:14:00 +0000

    Fixes #1, fixes #3

--- src/index.js
+++ src/index.js
@@ -3,64 +3,86 @@ import {logseq as PL} from "../package.json";
 import {settingUI} from './setting';
 
 const pluginId = PL.id;
-const pluginName = "Whatsapp plugin: "
+const pluginName = "LogLink plugin: "
 
 
-/* function to fetch data from LogWhat */
-async function loadWhatsappData() {
+/* function to fetch data from LogLink */
+async function loadRemoteData() {
 
-	const appURL = 'https://api.loglink.it';
-	const userID = logseq.settings["loglink_token"];
-	const endpoint = appURL + '/get_new_messages/';
+	// Set the URL to either the default or the user's custom URL
+	let appURL = 'https://api.loglink.it';
+	if (logseq.settings['loglink_server_url'] !== "") {
+		appURL = logseq.settings['loglink_server_url'];
+	}
+	let endpoint = appURL + '/get_new_messages/';
 
+	// Crete the results array, which we will use to return the responses
 	let results_array = [];
 
-	if (userID === "") {
+	// Work out how many tokens there are
+	let token_count = 0;
+	if (logseq.settings["loglink_token_1"] !== "") {
+		token_count = 1;
+	}
+	if (logseq.settings["loglink_token_2"] !== "") {
+		token_count = 2;
+	}
+	if (logseq.settings["loglink_token_3"] !== "") {
+		token_count = 3;
+	}
+
+	// Check that there is at least 1 token provided
+	if (token_count === 0) {
 		results_array.push("This plugin has not yet been initialised.")
 		results_array.push("In order to use this plugin, you must first authenticate with LogLink by visiting " + appURL);
 		results_array.push("Once you have authenticated, you will be given a token (a long string of letters and numbers). Enter that token in the LogLink plugin settings.");
 		return results_array
 	}
 
-	const object_to_send = {
-		'user_id': userID
-	}
+	// Iterate through the tokens
+	for (let i = 1; i <= token_count; i++) {
+		let userID = logseq.settings["loglink_token_" + i];
 
-	console.log(pluginName, "fetching data from ", endpoint)
-	console.log(pluginName, "sending data pacakge", object_to_send)
-
-	let response = await fetch(endpoint, {
-		method: 'POST',
-		body: JSON.stringify(object_to_send),
-		headers: {
-			'Content-type': 'application/json; charset=UTF-8'
+		let object_to_send = {
+			'user_id': userID
 		}
-	})
 
-	let data = await response.json();
-	let status = await response.status;
+		console.log(pluginName, "fetching data from ", endpoint)
+		console.log(pluginName, "sending data pacakge", object_to_send)
 
-	console.log(pluginName, "returned status code", status)
+		let response = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify(object_to_send),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		})
 
-	console.log(data.messages)
+		let data = await response.json();
+		let status = await response.status;
 
-	if (status === 200) {
-		let messages = data.messages.contents;
-		messages.forEach(function (item, index) {
-			results_array.push(item['contents']);
-			console.log(item['contents']);
-		});
+		console.log(pluginName, "returned status code", status)
 
-	} else {
-		results_array.push("There was an error fetching your data from LogLink. Error code was " + status + ".");
-		results_array.push(data.message)
-		console.log(data);
+		console.log(data.messages)
+
+		if (status === 200) {
+			let messages = data.messages.contents;
+			messages.forEach(function (item, index) {
+				results_array.push(item['contents']);
+				console.log(item['contents']);
+			});
+
+		} else {
+			results_array.push("There was an error fetching your data from LogLink. Error code was " + status + ".");
+			results_array.push(data.message)
+			console.log(data);
+		}
 	}
 
 	return results_array;
 }
 
-async function displayWhatsappData() {
+async function displayRemoteData() {
 
 
 	// Display the toast
@@ -77,8 +99,8 @@ async function displayWhatsappData() {
 
 	console.log(`#${pluginId}: ` + targetBlockUuid);
 
-	// Get the data from Whatsapp
-	let data = await loadWhatsappData();
+	// Get the data from the remote server
+	let data = await loadRemoteData();
 
 	// If the data is empty, display a message
 	if (data.length === 0) {
@@ -123,7 +145,7 @@ const main = async () => {
 		main`); /* -plugin-id */
 
 	logseq.Editor.registerSlashCommand('🔄 LogLink messages', async () => {
-			displayWhatsappData()
+			displayRemoteData()
 		}
 	);
 
--- src/setting.js
+++ src/setting.js
@@ -2,10 +2,31 @@ export const settingUI = () => {
     /* https://logseq.github.io/plugins/types/SettingSchemaDesc.html */
     const settingsTemplate = [
         {
-            key: "loglink_token",
+            key: "loglink_token_1",
             type: "string",
             title: "Your LogLink Token",
             description: "To link with LogLink, visit https://loglink.it/  Once you authenticate with LogLink, the site will generate a token. Enter it below.",
+            default: ""
+        },
+        {
+            key: "loglink_token_2",
+            type: "string",
+            title: "Your LogLink Token 2",
+            description: "If you have multiple tokens (for instance from multiple services) then you can enter an additional one here.",
+            default: ""
+        },
+        {
+            key: "loglink_token_3",
+            type: "string",
+            title: "Your LogLink Token 3",
+            description: "If you have multiple tokens (for instance from multiple services) then you can enter an additional one here.",
+            default: ""
+        },
+        {
+            key: "loglink_server_url",
+            type: "string",
+            title: "LogLink server URL",
+            description: "If you have self-deployed LogLink to your own server then enter the base URL below. If not, then leave this blank.",
             default: ""
         },
     ];
